9aaa5a75974e1ff292a62291a59cdfe4baaacbb4
Luca Santacatterina <s88.luca@gmail.com>
2015-07-24 07:39:06 +0200

    inserimento script perl visualizzazione tariffa singola

--- cgi-bin/plan.pl
+++ cgi-bin/plan.pl
@@ -26,11 +26,11 @@ my $node = $doc->getElementById("$id");
 
 # If not find redirect to error page, else inizialized web page
 if (!defined $node){
-  print $cgi->redirect('../404.html');
-  exit;
+	print $cgi->redirect('../404.html');
+	exit;
 }else{
-  print $cgi->header( -type => "text/html", charset => 'utf-8', -status => "200 OK" );
-  print $cgi->start_html;
+	print $cgi->header( -type => "text/html", charset => 'utf-8', -status => "200 OK" );
+	print $cgi->start_html;
 }
 
 
@@ -41,23 +41,23 @@ my $txt = encode_entities($desc);
 
 # Fill var to send an template file
 my $vars = {
-  operator        => $node->getName(),
-  id              => $node->findvalue('@xml:id'),
-  payments        => $node->findvalue('@payments'),
-  title           => $node->findvalue('title'),
-  insertdatetime  => $node->findvalue('insertdatetime'),
-  available       => $node->findvalue('available/@disabled'),
-  startdate       => $node->findvalue('available/startdate'),
-  enddate         => $node->findvalue('available/enddate'),
-  currency        => $node->findvalue('price/@currency'),
-  price           => $node->findvalue('price'),
-  unit            => $node->findvalue('expiry/@unit'),
-  expiry          => $node->findvalue('expiry'),
-  minutes         => $node->findvalue('rates/minutes'),
-  messages        => $node->findvalue('rates/messages'),
-  datasize        => $node->findvalue('rates/internet/@datasize'),
-  internet        => $node->findvalue('rates/internet'),
-  description     => "$txt"
+	operator		=> $node->getName(),
+	id				=> $node->findvalue('@xml:id'),
+	payments		=> $node->findvalue('@payments'),
+	title			=> $node->findvalue('title'),
+	insertdatetime	=> $node->findvalue('insertdatetime'),
+	available		=> $node->findvalue('available/@disabled'),
+	startdate		=> $node->findvalue('available/startdate'),
+	enddate			=> $node->findvalue('available/enddate'),
+	currency		=> $node->findvalue('price/@currency'),
+	price			=> $node->findvalue('price'),
+	unit			=> $node->findvalue('expiry/@unit'),
+	expiry			=> $node->findvalue('expiry'),
+	minutes			=> $node->findvalue('rates/minutes'),
+	messages		=> $node->findvalue('rates/messages'),
+	datasize		=> $node->findvalue('rates/internet/@datasize'),
+	internet		=> $node->findvalue('rates/internet'),
+	description		=> "$txt"
 };
 
 
